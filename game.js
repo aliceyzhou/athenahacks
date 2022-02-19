d3d87b10bef8414634a36ec58e4948d1897cc9ad
Andrea Gonzalez <andrea.gonz0012@gmail.com>
2022-02-19 14:26:52 -0800

    image slicer function

---
+++ game.js
@@ -0,0 +1,14 @@
+var image = new Image();
+
+image.src = '';
+
+// Function to slice image into a grid-like shape for the puzzle.
+// Parameters are num_rows and num_cols from user.
+
+function imageSlicer(num_rows, num_cols) {
+    for(var rows = 0; rows < num_rows; num_rows++) {
+        for(var cols = 0; cols < num_cols; num_cols++) {
+            
+        }
+    }
+}
